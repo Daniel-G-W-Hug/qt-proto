cbecd5ed02add0d0241bbe319acbe63e5ea5c7ef
Daniel Hug <daniel_hug@t-online.de>
2024-06-15 14:21:30 +0200

    einfachste Transformation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include "w_main.hpp"
+// #include "w_main.hpp"
 
 #include <QApplication>
+#include <QColor>
+#include <QGraphicsItem>
+#include <QGraphicsLineItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QtGui>
 
 #include <iostream>
 
+static constexpr int PPI = 254;  // MacOS Screenresolution in dots per inch
+static constexpr int PPCM = 100; // MacOS Screenresolution in dots per cm
 
 int main(int argc, char* argv[])
 {
@@ -11,10 +19,36 @@ int main(int argc, char* argv[])
     try {
         QApplication app(argc, argv);
 
-        w_main window;
+        QGraphicsView view;
 
-        window.setWindowTitle("qt Test");
-        window.show();
+        QGraphicsScene* scene = new QGraphicsScene(&view);
+        scene->setBackgroundBrush(Qt::yellow);
+
+        // scale screen resolution to get 100 pixels / cm
+        // scene->setSceneRect(QRectF(QPointF(-10, -10), QPointF(110, 110)));
+
+        view.setScene(scene);
+        // view.scale(1, -1);
+
+
+        QGraphicsLineItem* line1 = new QGraphicsLineItem(0, 0, 100, 100);
+        scene->addItem(line1);
+
+        QGraphicsRectItem* rect = new QGraphicsRectItem(0, 0, 100, 100);
+        scene->addItem(rect);
+
+        QGraphicsRectItem* rect2 = new QGraphicsRectItem(0, 0, 100, 100);
+        scene->addItem(rect2);
+
+        rect2->setRotation(15);
+
+        QGraphicsTextItem* text = new QGraphicsTextItem("Hello world!");
+        scene->addItem(text);
+
+
+        view.setWindowTitle("Template");
+        view.resize(400, 400);
+        view.show();
 
         return app.exec();
     }
